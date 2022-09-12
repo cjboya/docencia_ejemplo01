c42e380db07d91d24656856c66daaec9d94613c7
cjboya <cjboya@gmail.com>
2022-09-11 21:27:38 -0300

    final

--- ejercicio_nuevo.py
+++ ejercicio_nuevo.py
@@ -7,10 +7,3 @@ while x < 5:
     print("Hola "+nombre)
     x = x + 1
 
-print("Fin")
-print("Nuevo")
-print("agrego")
-
-print("rama b")
-
-git remote add origin https://github.com/cjboya/nuevo_repo.git
